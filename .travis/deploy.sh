6448dbfddcaf8b56a2ade431901667459dd043ae
Endboss <gituser@nothing.com>
2019-05-29 16:16:26 -0700

    attempt to cache go dependencies grabbed on prior run. also touch the restart file.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 scp -C -P 10022 goober goober@chws.ca:/shared/goober/
+scp -C -P 10022 restart goober@chws.ca:/shared/goober/
 #scp -C -P 10022 -r web goober@chws.ca:/shared/goober/
 rsync -avz -e "ssh -p 10022" web goober@chws.ca:/shared/goober/
+
